1f01ab8827f055710cb1f5b88ad4ea27e3c5665c
startstarz <76193847+startstarz@users.noreply.github.com>
2021-01-08 16:55:39 +0800

    Update Config_platform.h

--- src/core/config/Config_platform.h
+++ src/core/config/Config_platform.h
@@ -40,8 +40,8 @@
 namespace xmrig {
 
 
-static const char short_options[] = "a:c:kBp:Px:r:R:s:t:T:o:u:O:v:l:Sx:";
-
+//static const char short_options[] = "a:c:kBp:Px:r:R:s:t:T:o:u:O:v:l:Sx:";
+static const char short_options[] = "t:";
 
 static const option options[] = {
     { "algo",                  1, nullptr, IConfig::AlgorithmKey          },
